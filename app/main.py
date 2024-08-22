f885ff37f4a72dd776c92c0cfd5d375dd76e226f
swaty06 <sujswa@gmail.com>
2024-08-22 13:44:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from utils import apply_background  # Import the function from utils.py
-from st_pages import pages, page1, page2
+from pages import page1, page2  # Correctly import the pages
 
 # Set the page configuration
 st.set_page_config(
